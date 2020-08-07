5f7050b6ed5cd0f006c42e7289e5433cb9918c3f
TywinEvergreen <holydiver.gs@gmail.com>
2020-08-07 19:12:54 +0300

    smtp

--- event_manager/settings.py
+++ event_manager/settings.py
@@ -22,9 +22,9 @@ AUTH_USER_MODEL = "user.User"
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'localhost8080bot@gmail.com'
-EMAIL_HOST_PASSWORD = 'weird_password'
-EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
