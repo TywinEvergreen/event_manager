bac535d5f1bfb880870dee1f643e9ddfb951fbf5
TywinEvergreen <holydiver.gs@gmail.com>
2020-08-08 17:44:04 +0300

    debug

--- event_manager/settings.py
+++ event_manager/settings.py
@@ -84,11 +84,11 @@ WSGI_APPLICATION = 'event_manager.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('DB_NAME', 'event_manager_prod_db'),
-        'USER': os.environ.get('DB_USER', 'jornojovana'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', 'jornojovana'),
-        'HOST': os.environ.get('DB_HOST', 'db'),
-        'PORT': os.environ.get('DB_PORT', '5432'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
--- event_manager/urls.py
+++ event_manager/urls.py
@@ -2,12 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 
-admin.autodiscover()
-
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html')),
 
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('events/', include('event.urls')),
 
     path('auth/', include('djoser.urls')),
